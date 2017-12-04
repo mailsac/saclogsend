7707b9188283ec6c45d8856b3600d418da1f70a9
Jeff H. Parrish <jeffhparrish@gmail.com>
2017-12-04 12:13:55 -0800

    Initial commit of a remote syslogger shim

---
+++ slog.go
@@ -0,0 +1,37 @@
+package saclogsend
+
+import (
+	"fmt"
+	"log"
+	"log/syslog"
+)
+
+// Slog is a logger instance. All methods support Printf string replacement.
+type Slog struct {
+	log *syslog.Writer
+}
+
+// New creates a new logger and connects it to the remote host
+func New(tag, remoteHost string) (logger *Slog, err error) {
+	logger = &Slog{}
+	l, err := syslog.Dial("tcp", remoteHost, syslog.LOG_LOCAL0, tag)
+	logger.log = l
+	return logger, err
+}
+
+// Err writes an error message to the syslog
+func (slog *Slog) Err(message string, args ...interface{}) {
+	smsg := fmt.Sprintf(message, args...)
+	log.Println(smsg)
+	if slog.log != nil {
+		slog.log.Err(smsg)
+	}
+}
+
+// Info writes an informational message to the syslog
+func (slog *Slog) Info(message string, args ...interface{}) {
+	smsg := fmt.Sprintf(message, args...)
+	if slog.log != nil {
+		slog.log.Info(smsg)
+	}
+}
